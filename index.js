2943c79df44f406cea4ca123ee741006bfd2cee3
Keylane <keylanessilva07@gmail.com>
2022-02-05 04:34:11 -0300

    gerador de senha

--- index.js
+++ index.js
@@ -1,36 +1,90 @@
-var senhaAleatoria = ''
+let sliderElement1 = document.querySelector("#slider1")
+let sliderElement2 = document.querySelector("#slider2")
+let sliderElement3 = document.querySelector("#slider3")
+let sliderElement4 = document.querySelector("#slider4")
 
-function gerarAleatorio(qtd_num, qtd_mai, qtd_min, qtd_simb){
+let buttonElement = document.querySelector("#button")
+
+let tamanhoSenha1 = document.querySelector("#valor1")
+let tamanhoSenha2 = document.querySelector("#valor2")
+let tamanhoSenha3 = document.querySelector("#valor3")
+let tamanhoSenha4 = document.querySelector("#valor4")
+
+let senha = document.querySelector("#senha")
+
+let containerSenha = document.querySelector("#container-senha")
+
+let novaSenha = ''
+
+tamanhoSenha1.innerHTML = sliderElement1.value;
+sliderElement1.oninput = function(){
+    tamanhoSenha1.innerHTML = this.value
+}
+
+tamanhoSenha2.innerHTML = sliderElement2.value;
+sliderElement2.oninput = function(){
+    tamanhoSenha2.innerHTML = this.value
+}
+
+tamanhoSenha3.innerHTML = sliderElement3.value;
+sliderElement3.oninput = function(){
+    tamanhoSenha3.innerHTML = this.value
+}
+
+tamanhoSenha4.innerHTML = sliderElement4.value;
+sliderElement4.oninput = function(){
+    tamanhoSenha4.innerHTML = this.value
+}
+
+function gerarSenha(){
+    containerSenha.classList.remove("hide")
+
+    var senhaAleatoria = []
+    var rec = ''
 
     let numerico = '0123456789'
-    for(let i  = 0; i < qtd_num; i++){
-        senhaAleatoria+= Math.floor(Math.random() * numerico.length)
+    for(let i  = 0; i < sliderElement1.value; i++){
+        senhaAleatoria.push(Math.floor(Math.random() * numerico.length))
+        console.log('Atum' + senhaAleatoria[i])
     }
 
     let maiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for(let i = 0; i < qtd_mai; i++ ){
-        senhaAleatoria+= maiusculas.charAt((Math.floor(Math.random() * maiusculas.length)))
+    for(let i = 0; i < sliderElement2.value; i++ ){
+        senhaAleatoria.push(maiusculas.charAt((Math.floor(Math.random() * maiusculas.length))))
     }
 
     let minusculas = 'abcdefghijklmnopqrstuvwxyz'
-    for(let i = 0; i < qtd_min; i++ ){
-        senhaAleatoria+= minusculas.charAt((Math.floor(Math.random() * minusculas.length)))
+    for(let i = 0; i < sliderElement3.value; i++ ){
+        senhaAleatoria.push(minusculas.charAt((Math.floor(Math.random() * minusculas.length))))
     }
     let simbolos = '!@#$%?'
-    for(let i = 0; i < qtd_simb; i++){
-        senhaAleatoria+= simbolos.charAt((Math.floor(Math.random() * simbolos.length)))
+    for(let i = 0; i < sliderElement4.value; i++){
+        senhaAleatoria.push(simbolos.charAt((Math.floor(Math.random() * simbolos.length))))
     }
-    console.log(senhaAleatoria)
+
+    
+    function embaralharSenha(){
+        var p,n, tmp
+        for(p = senhaAleatoria.length; p;){
+            n = Math.random() * p-- | 0;
+            tmp = senhaAleatoria[n]
+            senhaAleatoria[n] = senhaAleatoria[p]
+            senhaAleatoria[p] = tmp
+        }
+        for(let i = 0; i < senhaAleatoria.length; i ++){
+            rec += senhaAleatoria[i]
+            // console.log(senhaAleatoria[i])
+        }   
+    }
+    // console.log("Rec: " + rec)
+    embaralharSenha()
+    senha.innerHTML = rec
+    novaSenha = rec
     
 }
 
-function embaralharSenha(){
-    var novaSenha = ''
-    for(let i = 0; i < 10; i++){
-        novaSenha+= senhaAleatoria.charAt(Math.floor(Math.random() * senhaAleatoria.length))
-    }
-    console.log(novaSenha)
+function copySenha(){
+    alert('Senha copiada: ' + novaSenha)
+    navigator.clipboard.writeText(novaSenha)
 }
 
-gerarAleatorio(2,3,4,2)
-embaralharSenha()
